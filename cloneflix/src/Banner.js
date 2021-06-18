2af0600738ecdeecb8b7e94deb12c952e559f8be
joshua <sjpk1223@gmail.com>
2021-06-18 14:13:44 -0400

    Truncate description complete

--- cloneflix/src/Banner.js
+++ cloneflix/src/Banner.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "./axios";
+import "./Banner.css";
 
 function Banner({ fetchUrl }) {
   // background img, movie title, 2 buttons(play, myList),description
@@ -17,9 +18,29 @@ function Banner({ fetchUrl }) {
     fetchData();
   }, []);
   console.log(movie);
+  // adds ... to the end of description if its too long
+  function truncate(str, n) {
+    return str?.length > n ? str.substr(0, n - 1) + "..." : str;
+  }
   return (
-    <header>
-      <div></div>
+    <header
+      className="header_container"
+      style={{
+        backgroundSize: "cover",
+        backgroundImage: `url(https://image.tmdb.org/t/p/original/${movie?.backdrop_path})`,
+        backgroundPosition: "center center",
+      }}
+    >
+      <div className="content_container">
+        <h1 className="header_title">
+          {movie?.title || movie?.name || movie?.original_name}
+        </h1>
+        <div className="button_container">
+          <button className="header_button">Play</button>
+          <button className="header_button">My List</button>
+        </div>
+        <h1 className="header_description">{truncate(movie?.overview, 150)}</h1>
+      </div>
     </header>
   );
 }
